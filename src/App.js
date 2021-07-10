8e031484e4feb14d66ab6d54a528002408ada395
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-07-10 19:32:24 +0430

    use BlogDetail component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './components/Home';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './components/Create';
+import Detail from './components/BlogDetail';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/"><Home /></Route>
           <Route exact path="/create"><Create /></Route>
+          <Route exact path="/post/:id"><Detail /></Route>
         </Switch>
       </div>
     </Router>
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ title, posts }) => {
     return (
         <div className="blog-list">
             { title && <div className="blog-list-title">{ title }</div> }
             { posts.map((post) =>
                 <div className="post-card" key={ post.id }>
-                    <h4>{ post.title.slice(0, 35) }</h4>
-                    <p>{ post.body.slice(0, 150) }</p>
+                    <Link to={ `/post/${post.id}` }>
+                        <h4>{ post.title.slice(0, 35) }</h4>
+                        <p>{ post.body.slice(0, 150) }</p>
+                    </Link>
                 </div>
             )}
         </div>
