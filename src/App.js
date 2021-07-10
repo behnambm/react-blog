6a9b1780fe759e21df4d3fd358627ef35192e865
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-07-10 17:54:04 +0430

    Use routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Home from './components/Home';
 import Header from './components/Header';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './components/Create';
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <Switch>
+          <Route exact path="/"><Home /></Route>
+          <Route exact path="/create"><Create /></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
