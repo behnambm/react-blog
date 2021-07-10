416dfee8831ebb7ef6d37abe6fb6483eb6cdc10b
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-07-10 11:53:18 +0430

    Add header component with some style

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>Blog</h1> 
+            <ul>
+                <li>Home</li>
+                <li>Create</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default Header;
