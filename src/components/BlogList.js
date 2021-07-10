aa62d8cf2a4dde4edc2a47edc43803f93ac1af04
Behnam Mohammadzadeh <behnam.mohamadzadeh21@gmail.com>
2021-07-10 13:11:32 +0430

    Add BlogList component
    BlogList component is used to list all of the `posts` the are passed to &
    this component takes a `title` prop too

---
+++ src/components/BlogList.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+const BlogList = ({ title, posts }) => {
+    return (
+        <div className="blog-list">
+            { title && <div className="blog-list-title">{ title }</div> }
+            { posts.map((post) =>
+                <div className="post-card">
+                    <h4>{ post.title.slice(0, 35) }</h4>
+                    <p>{ post.body.slice(0, 150) }</p>
+                </div>
+            )}
+        </div>
+    );
+}
+ 
+export default BlogList;
